afe4f73e5c18e57518dd0230b333221ec9f6670a
Tim <sarrtim@gmail.com>
2019-03-21 20:49:58 +0300

    Update username param to email

--- src/_services/user.service.js
+++ src/_services/user.service.js
@@ -13,18 +13,18 @@ export const userService = {
     delete: _delete
 };
 
-function login(username, password) {
+function login(email, password) {
     const requestOptions = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ username, password })
+        body: JSON.stringify({ email, password })
     };
 
     return fetch(`${config.apiUrl}/account/generate/jwt/`, requestOptions)
         .then(handleResponse)
         .then(user => {
             // login successful if there's a jwt token in the response
-            if (user.token) {
+            if (user.access_token) {
                 // store user details and jwt token in local storage to keep user logged in between page refreshes
                 localStorage.setItem('user', JSON.stringify(user));
             }
