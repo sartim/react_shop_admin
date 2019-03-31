dac0ebd79a7b7332f5d59c092c904efa47beb6e8
Tim <sarrtim@gmail.com>
2019-03-31 21:21:21 +0300

    Add products & products reducers to combineReducers

--- src/_reducers/index.js
+++ src/_reducers/index.js
@@ -4,10 +4,13 @@ import { registration } from './registration.reducer';
 import { users, user, online_users } from './users.reducer';
 import { alert } from './alert.reducer';
 import {search_results} from "./search.reducer";
+import {product, products} from "./products.reducer";
 
 const rootReducer = combineReducers({
   authentication,
   registration,
+  product,
+  products,
   user,
   users,
   online_users,
