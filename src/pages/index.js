b5225522dec2622159b4389b5b979e88cc423698
Tim <sarrtim@gmail.com>
2019-04-23 20:50:52 +0300

    Update index

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,4 @@ export * from './HeaderPage/HeaderPage';
 export * from './LoginPage/LoginPage';
 export * from './RegisterPage/RegisterPage';
 export * from './UserListPage/UserListPage';
-export * from './ArticleCreatePage/ArticleCreatePage';
-export * from './ArticleCreateEditorPage/ArticleCreateEditorPage';
-export * from './ArticleListPage/ArticleListPage';
-export * from './SideNavPage/SideNavPage';
+export * from './LeftSideNavPage/LeftSideNavPage';
