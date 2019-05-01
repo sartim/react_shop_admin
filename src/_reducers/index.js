43aa73c2bfe46cb4d7b2c58b3ee9d0bf8ca20562
Tim <sarrtim@gmail.com>
2019-05-01 19:34:50 +0300

    AddAdd order reducer to combineReducers

--- src/_reducers/index.js
+++ src/_reducers/index.js
@@ -3,12 +3,14 @@ import { authentication } from './authentication.reducer';
 import { registration } from './registration.reducer';
 import { users, user, online_users } from './users.reducer';
 import { alert } from './alert.reducer';
-import {search_results} from "./search.reducer";
-import {product, products} from "./products.reducer";
+import { search_results } from "./search.reducer";
+import { product, products } from "./products.reducer";
+import { order } from "./orders.reducer";
 
 const rootReducer = combineReducers({
   authentication,
   registration,
+  order,
   product,
   products,
   user,
